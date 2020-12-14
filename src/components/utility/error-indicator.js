683c40e8e762318ba7aad2482900d76127c2e0b5
zhebil <jbilyk1996@gmail.com>
2020-12-14 22:00:00 +0200

    error message

--- src/components/utility/error-indicator.js
+++ src/components/utility/error-indicator.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from "react";
 export default function ErrorIndicator() {
-    return (
-        <div>
-            Error
-        </div>
-    )
+  return (
+    <div className="error">
+      <div className="container">
+        <h1 className="title error__title">Ошибка!</h1>
+        <p className="error__message">
+          Что-то пошло не так. Попробуйте позже и{" "}
+          <a href="https://t.me/zhebil" className="link">
+            сообщите об ошибке
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
